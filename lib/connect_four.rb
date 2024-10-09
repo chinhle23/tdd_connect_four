7754630dd0f091321e08a550070257adabc1eef5
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-10-09 05:17:11 -0400

    feat: define #read_input

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -8,6 +8,7 @@ class ConnectFour
     ['☐', '☐', '☐', '☐', '☐', '☐', '☐']
   ])
     @board = board
+    @current_piece = '☑'
   end
 
   def display(board=@board)
@@ -26,6 +27,20 @@ class ConnectFour
     lowest_empty_spot = 5
     @board.each_with_index { |row, index| lowest_empty_spot = index if row[column] == '☐' }
     @board[lowest_empty_spot][column] = piece
+  end
+
+  def read_input
+    column_input = gets.chomp
+
+    exit if column_input == 'exit' || column_input == 'q'
+
+    if column_input.to_i.to_s != column_input || column_input.to_i > 7 || column_input.to_i < 1 
+      puts 'Invalid input: Please input a number between 1 and 7'
+      return 
+    end
+
+    drop_piece(column_input.to_i, @current_piece)
+    @current_piece = @current_piece == '☑' ? '☒' : '☑'
   end
 end
   
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -62,4 +62,115 @@ describe ConnectFour do
       end
     end
   end
+
+  describe '#read_input' do
+    before do
+      valid_input = '3'
+      allow(game).to receive(:gets).and_return(valid_input)
+    end
+
+    context 'when waiting for column input' do
+      it 'receives input with gets' do
+        expect(game).to receive(:gets)
+        game.read_input
+      end
+
+      it 'calls #drop_piece with column input' do
+        expect(game).to receive(:drop_piece).with(3, '☑')
+        game.read_input
+      end
+    end
+
+    context 'when it is the first turn' do
+      it 'calls #drop_piece with ☑' do
+        expect(game).to receive(:drop_piece).with(3, '☑')
+        game.read_input
+      end
+    end
+
+    context 'when it is the second turn' do
+      it 'calls #drop_piece with ☒' do
+        game.read_input
+        expect(game).to receive(:drop_piece).with(3, '☒')
+        game.read_input
+      end
+    end
+
+    context 'when user inputs exit' do
+      before do
+        allow(game).to receive(:gets).and_return('exit')
+      end
+
+      it 'exits the game' do
+        expect(game).to receive(:exit)
+        game.read_input
+      end
+    end
+
+    context 'when user inputs q' do
+      before do
+        allow(game).to receive(:gets).and_return('q')
+      end
+
+      it 'exits the game' do
+        expect(game).to receive(:exit)
+        game.read_input
+      end
+    end
+
+    describe 'invalid inputs' do
+      before do
+        allow(game).to receive(:gets).and_return('peanut')
+      end
+
+      it 'does not call #drop_piece' do
+        expect(game).not_to receive(:drop_piece)
+        game.read_input
+      end
+
+      context 'when user inputs letter' do
+        before do
+          allow(game).to receive(:gets).and_return('a')
+        end
+
+        it 'displays error message' do
+          expect(game).to receive(:puts).with('Invalid input: Please input a number between 1 and 7')
+          game.read_input
+        end
+      end
+
+      context 'when user inputs a large number' do
+        before do
+          allow(game).to receive(:gets).and_return('25')
+        end
+
+        it 'displays error message' do
+          expect(game).to receive(:puts).with('Invalid input: Please input a number between 1 and 7')
+          game.read_input
+        end
+      end
+
+      context 'when user inputs zero' do
+        before do
+          allow(game).to receive(:gets).and_return('0')
+        end
+
+        it 'displays error message' do
+          expect(game).to receive(:puts).with('Invalid input: Please input a number between 1 and 7')
+          game.read_input
+        end
+      end
+
+      context 'when user inputs a negative number' do
+        before do
+          allow(game).to receive(:gets).and_return('-1')
+        end
+
+        it 'displays error message' do
+          expect(game).to receive(:puts).with('Invalid input: Please input a number between 1 and 7')
+          game.read_input
+        end
+      end
+    end
+  end
 end
